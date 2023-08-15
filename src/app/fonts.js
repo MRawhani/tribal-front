85019d182f42bacea994148c0af620fa34459cb4
mrawhani <mrawhani5@gmail.com>
2023-08-15 04:37:01 -0700

    fix fonts

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,12 +1,31 @@
 import { Open_Sans } from 'next/font/google'
 import localFont from 'next/font/local'
 // Font files can be colocated inside of `pages`
-const Pecita = localFont({
-    src: '../../public/fonts/Pecita.otf',
-    variable: '--font-pecita',
+// const Pecita = localFont({
+//     src: '../../public/fonts/Pecita.otf',
+//     variable: '--font-pecita',
+    
+//     display: 'swap'
+//   })
+const GothamBold = localFont({
+    src: '../../public/fonts/Gotham-Bold.otf',
+    variable: '--font-gothamBold',
     
     display: 'swap'
   })
+const GothamMedium = localFont({
+    src: '../../public/fonts/GothamMedium.ttf',
+    variable: '--font-gothamMedium',
+    
+    display: 'swap'
+  })
+  const NCS = localFont({
+    src: '../../public/fonts/NCS Radhiumz.ttf',
+    variable: '--font-ncs',
+    
+    display: 'swap'
+  })
+ 
   const GothamBook = localFont({
     src: '../../public/fonts/GothamBook.ttf',
     variable: '--font-gothamBook',
@@ -19,13 +38,8 @@ const Pecita = localFont({
     
     display: 'swap'
   })
-  const NCS = localFont({
-    src: '../../public/fonts/NCS Radhiumz.ttf',
-    variable: '--font-ncs',
-    
-    display: 'swap'
-  })
+ 
   
   const open_Sans = Open_Sans({ subsets: ['latin'] })
 
-  export const custom_fonts = ` ${Pecita.variable} ${GothamLight.variable} ${NCS.variable} ${GothamBook.variable}`
+  export const custom_fonts = `${GothamMedium.variable} ${GothamBold.variable} ${GothamLight.variable} ${NCS.variable} ${GothamBook.variable}`
