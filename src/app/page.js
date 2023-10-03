f2beff68fdae3d8ca13877de6a3eec0165093073
Naif Sameer <nayef7138@gmail.com>
2023-10-03 22:45:28 +0300

    :art: update: app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,17 @@
 import Hero from '@/components/home/Hero'
 import About from '@/components/home/About'
-import Image from 'next/image'
 import QuickLinks from '@/components/home/QuickLinks'
 import Articles from '@/components/home/Articles'
 import Reports from '@/components/home/Reports'
 
-
-export default async function  Home() {
-  
+export default async function Home() {
   return (
-    <main > 
-     <Hero />
-     <About />
-     <QuickLinks />
-     <Articles />
-     <Reports />
- 
-
+    <main>
+      <Hero />
+      <About />
+      <QuickLinks />
+      <Articles />
+      <Reports />
     </main>
   )
 }
