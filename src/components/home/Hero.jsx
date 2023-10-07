b2a0cfc434f69458eaedb67353c7db03da7b40b8
Naif Sameer <nayef7138@gmail.com>
2023-10-07 22:17:25 +0300

    :sparkles: update: home/hero section

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,45 +1,19 @@
-import Image from "next/image";
+import { homeData } from "@/utils/data";
 import React from "react";
 
 export default function Hero() {
   return (
-    <div className="container">
+    <div className="hero ">
+      <div className="container">
+        <div className="hero__content">
+          <h1 className="hero__title">{homeData.heroSection.title}</h1>
 
-    <div className="hero flex flex-col items-center justify-center">
-      <div className="hero__content flex flex-col items-center justify-center">
-        <div className="hero__text flex flex-col items-center justify-center">
-          <span>Greeting, Guests</span>
-          <h1>Yemen Tribal Voices</h1>
-          {/* <h2>Hoope of Yemenies</h2> */}
-          <div className="relative ">
-          <Image
-            src="/icons/arabic-title.svg" 
-            alt="mouse-arrow"
-            width="0"
-            height="0"
-            className="w-auto h-10 2xl:h-14 3xl:h-16"
-          />
-          </div>
-          <p>
-            Discover the resounding echoes of
-            <br />
-             <strong>Yemeni tribes</strong> at their finest on
-            our platform.
+          <p className="hero__description">
+            {homeData.heroSection.description}
           </p>
         </div>
-        <div className="hero__arrow flex  items-center justify-center">
-          <div className="relative ">
-          <Image
-            src="/icons/mouse-arrow.svg" 
-            alt="mouse-arrow"
-            width="0"
-            height="0"
-            className="w-auto h-14 2xl:h-20"
-          />
-          </div>
-        </div>
       </div>
-    </div>
+      <div className="hero__arrow"></div>
     </div>
   );
 }
---
+++ src/utils/data/home.js
@@ -0,0 +1,8 @@
+export const homeData = {
+  heroSection: {
+    title: "Hello, Guests! 👋",
+    description:
+      "Discover the resounding echoes of Yemeni tribes at their finest on our platform.",
+    backgroundImage: "",
+  },
+};
