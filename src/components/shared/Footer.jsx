73813c8bf96f1f8d505e434ec434645c07f9066d
Naif Sameer <nayef7138@gmail.com>
2023-10-24 20:50:42 +0300

    :sparkles: update: footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import LogoIcon from "../icons/LogoIcon";
+import { globalData } from "@/utils/data";
+import Link from "next/link";
+
+const FooterLinks = () => {
+  return globalData.footerLinks.map((link) => (
+    <div key={link.title}>
+      <Link
+        className="py-2 px-3 flex justify-between items-center gap-2 bg-white rounded-3xl"
+        href={link.link}
+      >
+        <span className="text-xs font-gothamBold">{link.title}</span>
+        <Image src="/icons/arrow-link.svg" alt="icon" width={12} height={12} />
+      </Link>
+    </div>
+  ));
+};
 
 export default function Footer() {
   return (
-    <div className='h-96 container pt-60'>Footer</div>
-  )
+    <div className="footer ">
+      <div className="container pt-16 pb-8">
+        <div className="flex items-center flex-col text-white">
+          <LogoIcon />
+          <p className="font-gothamLight text-xs leading-4 mt-6 text-center">
+            Conflict & political Analyst. Community Development Practitioner.
+            Yemen. Tribes. Author of “Foe Not Friend: Yemeni Tribes & AQAP”
+          </p>
+        </div>
+
+        <div className="flex flex-wrap gap-2 md:justify-center mt-11 ">
+          <FooterLinks />
+        </div>
+
+        {/* social media */}
+        <div className="flex justify-center gap-3 mt-11">
+          {globalData.socialMediaLinks.map((link) => (
+            <div key={link.title}>
+              <a href={link.link} target="_blank" rel="noreferrer">
+                <Image src={link.icon} alt="icon" width={24} height={24} />
+              </a>
+            </div>
+          ))}
+        </div>
+
+        {/* copyright */}
+        <p className="text-center text-white text-xs mt-6 font-gothamLight">© All rights reserved Nadwa Dawsar</p>
+      </div>
+    </div>
+  );
 }
--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -1,1 +1,19 @@
 export * from './home'
+
+export const globalData = {
+    footerLinks: [
+        {title: 'Articles', link: '/articles'},
+        {title: 'Reports', link: '/reports'},
+        {title: 'Policy Papers', link: '/policy-papers'},
+        {title: 'Book chapters', link: '/book-chapters'},
+        {title: 'Interviews', link: '/interviews'},
+        {title: 'Unpublished Research', link: '/unpublished-research'},
+        {title: 'Contact me', link: '/contact-me'},
+        {title: 'About', link: '/about'},
+    ],
+    socialMediaLinks: [
+        { title: 'LinkedIn',icon: '/icons/linkedin.svg', link: '#' },
+        { title: 'Youtube',icon: '/icons/youtube.svg', link: '#'},
+        {title: 'Facebook',icon: '/icons/facebook.svg', link: '#'},
+    ]
+}
