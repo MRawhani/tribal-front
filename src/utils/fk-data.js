1166005899445c5f80d0bbb6741fd86ec22f138c
Naif Sameer <nayef7138@gmail.com>
2023-10-28 20:56:30 +0300

    :sparkles: update: stories

--- src/utils/fk-data.js
+++ src/utils/fk-data.js
@@ -48,3 +48,54 @@ export const articles = [
         recource_icon: 'recource3'
     },
 ]
+
+export const stories = [
+    {
+        id:1,
+        img:'article1',
+        title: 'The Biden Administration needs a Yemen policy',
+        date:'26 Feb 2023',
+        recource_name: 'Middle East Institute',
+        recource_icon: 'recource1'
+    },
+    {
+        id:2,
+        img:'article2',
+        title: 'Yemen’s Prisoner Exchange must be depolarized',
+        date:'26 Feb 2023',
+        recource_name: 'Sanaa Center for Strategic Studies',
+        recource_icon: 'recource2'
+    },
+    {
+        id:3,
+        img:'article3',
+        title: 'The Houthis’ End Game in Yemen, Al-Jazeera',
+        date:'26 Feb 2023',
+        recource_name: 'Al-Jazeera English',
+        recource_icon: 'recource3'
+    },
+    {
+        id:4,
+        img:'article1',
+        title: 'The Biden Administration needs a Yemen policy',
+        date:'26 Feb 2023',
+        recource_name: 'Middle East Institute',
+        recource_icon: 'recource1'
+    },
+    {
+        id:5,
+        img:'article2',
+        title: 'Yemen’s Prisoner Exchange must be depolarized',
+        date:'26 Feb 2023',
+        recource_name: 'Sanaa Center for Strategic Studies',
+        recource_icon: 'recource2'
+    },
+    {
+        id:6,
+        img:'article3',
+        title: 'The Houthis’ End Game in Yemen, Al-Jazeera',
+        date:'26 Feb 2023',
+        recource_name: 'Al-Jazeera English',
+        recource_icon: 'recource3'
+    },
+]
