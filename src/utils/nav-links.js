3616995ef67d48d6f76d423cde5bf888a4b69700
Naif Sameer <nayef7138@gmail.com>
2023-11-05 00:01:05 +0300

    :sparkles: update: nav-links file

--- src/utils/nav-links.js
+++ src/utils/nav-links.js
@@ -1,111 +1,100 @@
 export const links = [
-
-    {
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "My Writing",
+    id: 3,
+    children: [
+      {
         id: 1,
-        name: 'Home',
-        path: '/',
-       
-    },
-    {
+        name: "Unpublished research",
+        path: "/unpublished-research",
+      },
+      {
         id: 2,
-        name: 'About',
-        path: '/about',
-       
-    },
-    {
-       
-        name: 'My Writing',
+        name: "Articles",
+        path: "/articles",
+      },
+      {
         id: 3,
-        children: [
-            {
-                id: 1,
-                name: 'Books',
-                path: '/books',
-               
-            },
-            {
-                id: 2,
-                name: 'Articles',
-                path: '/articles',
-               
-            },
-            {
-                id: 3,
-                name: 'Books',
-                path: '/books',
-               
-            },
-        ],
-       
-    },
-    {
-        id: 4,
-        name: 'Interviews',
-        path: '/interviews',
-       
-    },
-    {
-        id: 5,
-        name: 'Stories',
-        path: '/stories',
-       
-    },
-    {
-        id: 6,
-        name: 'Contact Me',
-        path: '/contact-me',
-       
-    }
-]
+        name: "Books",
+        path: "/book-chapters",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Interviews",
+    path: "/interviews",
+  },
+  {
+    id: 5,
+    name: "Stories",
+    path: "/stories",
+  },
+  {
+    id: 6,
+    name: "Contact Me",
+    path: "/contact-me",
+  },
+];
 
 export const short_links = [
-    {
-        id: 1,
-        name: 'Articles',
-        icon: 'articles',
-        count:24,
-        path: "/"
-    },
-    {
-        id: 2,
-        name: 'Reports',
-        icon: 'reports',
-        count:8,
-        path: "/"
-    },
-    {
-        id: 3,
-        name: 'Book chapters',
-        icon: 'books',
-        count:2,
-        path: "/"
-    },
-    {
-        id: 4,
-        name: 'Interviews',
-        icon: 'interviews',
-        count:12,
-        path: "/"
-    },
-    {
-        id: 5,
-        name: 'Policy Papers',
-        icon: 'policy',
-        count:14,
-        path: "/"
-    },
-    {
-        id: 6,
-        name: 'Unpublished Research',
-        icon: 'unpublished',
-        count:7,
-        path: "/"
-    },
-    {
-        id: 7,
-        name: 'Stories',
-        icon: 'stories',
-        count:24,
-        path: "/"
-    },
-]
+  {
+    id: 1,
+    name: "Articles",
+    icon: "/icons/articles.svg",
+    count: 24,
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "Reports",
+    icon: "/icons/reports.svg",
+    count: 8,
+    path: "/",
+  },
+  {
+    id: 3,
+    name: "Book chapters",
+    icon: "/icons/books.svg",
+    count: 2,
+    path: "/",
+  },
+  {
+    id: 4,
+    name: "Interviews",
+    icon: "/icons/interviews.svg",
+    count: 12,
+    path: "/",
+  },
+  {
+    id: 5,
+    name: "Policy Papers",
+    icon: "/icons/policy.svg",
+    count: 14,
+    path: "/",
+  },
+  {
+    id: 6,
+    name: "Unpublished Research",
+    icon: "/icons/unpublished.svg",
+    count: 7,
+    path: "/",
+  },
+  {
+    id: 7,
+    name: "Stories",
+    icon: "/icons/stories.svg",
+    count: 24,
+    path: "/",
+  },
+];
