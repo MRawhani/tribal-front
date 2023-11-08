7e88e8366af59f025b781c7af2dba3695aca3378
Naif Sameer <nayef7138@gmail.com>
2023-11-08 12:40:11 +0300

    :lipstick: update: animation ⭐️
    - report card hover effect

--- src/components/global/ReportCard.jsx
+++ src/components/global/ReportCard.jsx
@@ -3,12 +3,8 @@ import Link from "next/link";
 
 export default function ReportCard({ report, className = "" }) {
   return (
-    <div
-      className={
-        "c-report-card grid grid-cols-12 gap-x-3 shadow-sm " + className
-      }
-    >
-      <div className="col-span-4">
+    <div className={"c-report-card flex shadow-sm " + className}>
+      <div className="w-4/12 c-report-card__img-wrapper">
         <div className="c-report-card__img">
           <Image
             src={`/assets/${report.img}.png`}
@@ -20,32 +16,58 @@ export default function ReportCard({ report, className = "" }) {
         </div>
       </div>
 
-      <div className="col-span-8 pe-3 md:pe-0 md:col-span-7 py-4 c-report-card__content">
-        <p className="c-report-card__date">{report.date}</p>
+      <div className="w-8/12 px-3 md:w-7/12 py-4 c-report-card__content-wrapper">
+        <div className="c-card__content">
+          <p className="c-report-card__date">{report.date}</p>
 
-        <Link
-          className="c-report-card__title line-clamp-2"
-          href={`/reports/${report.id}`}
-        >
-          {report.title}
-        </Link>
+          <Link
+            className="c-report-card__title line-clamp-2"
+            href={`/reports/${report.id}`}
+          >
+            {report.title}
+          </Link>
 
-        <div className="c-report-card__recource flex items-center">
-          <div className="c-report-card__recource-icon">
-            <Image
-              src={`/assets/${report.recource_icon}.svg`}
-              alt={`report`}
-              fill
-            />
+          <div className="c-report-card__recource flex items-center">
+            <div className="c-report-card__recource-icon">
+              <Image
+                src={`/assets/${report.recource_icon}.svg`}
+                alt={`report`}
+                fill
+              />
+            </div>
+            <h6 className="c-report-card__recource-name mt-3 line-clamp-1">
+              {report.recource_name}
+            </h6>
           </div>
-          <h6 className="c-report-card__recource-name mt-3 line-clamp-1">
-            {report.recource_name}
-          </h6>
+        </div>
+
+        <div className="c-card__content-effect">
+          <p className="c-report-card__date">{report.date}</p>
+
+          <Link
+            className="c-report-card__title line-clamp-2"
+            href={`/reports/${report.id}`}
+          >
+            {report.title}
+          </Link>
+
+          <div className="c-report-card__recource flex items-center">
+            <div className="c-report-card__recource-icon">
+              <Image
+                src={`/assets/${report.recource_icon}.svg`}
+                alt={`report`}
+                fill
+              />
+            </div>
+            <h6 className="c-report-card__recource-name mt-3 line-clamp-1">
+              {report.recource_name}
+            </h6>
+          </div>
         </div>
       </div>
 
       <Link
-        className="col-span-2 md:col-span-1 c-report-card__link"
+        className="md:w-1/12 c-report-card__link"
         href={`/reports/${report.id}`}
       >
         <Image
