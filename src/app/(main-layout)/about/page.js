c0a2c14e4ad006d0b9354b3298c9e0139655b215
Naif Sameer <nayef7138@gmail.com>
2023-11-15 02:15:25 +0300

    :lipstick: fix: about us page padding

--- src/app/(main-layout)/about/page.js
+++ src/app/(main-layout)/about/page.js
@@ -5,7 +5,7 @@ import { Breadcrumb } from "@/components/global/Breadcrumb";
 
 export default function AboutPage() {
   return (
-    <div className="page-about">
+    <div className="page-about pb-48 md:pb-40 lg:pb-8">
       <div className="container pb-8 md:pb-40 ">
         <Breadcrumb
           links={[{ title: "about us", href: "/about" }]}
