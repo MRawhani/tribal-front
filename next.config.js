349f65583fb6b20ac9b10f17a50efb71d8a06087
Naif Sameer <nayef7138@gmail.com>
2024-01-18 22:44:54 +0300

    :sparkles: update: connect with the api and update story details page

--- next.config.js
+++ next.config.js
@@ -1,11 +1,37 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
+const path = require("path");
 const nextConfig = {
-
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    domains: ["https://tribal-api.onrender.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value: `
+          //   default-src self;
+          //   script-src * 'unsafe-inline' 'unsafe-eval';
+          //   style-src * 'unsafe-inline';
+          //   img-src * data:;
+          //   font-src *;
+          //   connect-src *;
+          // `,
+          // },
+        ],
       },
-}
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
